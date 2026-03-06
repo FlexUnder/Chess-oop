908c377885cde5dbbff5cc2f99bafbc3672ae2a4
flexander <vorobeysas1@gmail.com>
2026-03-07 00:01:56 +0300

    Hex deleted trio added

--- base.py
+++ base.py
@@ -6,7 +6,7 @@ BLACK_SQUARE = "\033[38;5;0;48;5;141m"
 BLACK_PIECE_COLOR = "\x1b[30m"
 WHITE_PIECE_COLOR = "\033[38;5;255m"
 
-numbers_to_str_variants = {'1': 'classic', '2': 'hex', '3': 'mods', '4': 'checkers'}
+numbers_to_str_variants = {'1': 'classic', '2': 'trio', '3': 'mods', '4': 'checkers'}
 numbers_to_str_modes = {'1': 'online', '2': 'local', '3': 'back'}
 network_menu = {'1': 'server', '2': 'client', '3': 'back'}
 connection_menu = {'q': 'back'}
